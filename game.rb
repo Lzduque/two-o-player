69ba3ba011255bc1e4011249cae3625a5456f12d
Lzduque <lzduque@hotmail.com>
2019-05-14 13:05:56 +0000

    random questions generate

--- game.rb
+++ game.rb
@@ -1,24 +1,19 @@
 require "./player"
 
 class Game
-  attr_accessor :player1, :player2, :turn
+  attr_accessor :player1, :player2, :turn, :first_num, :second_num, :result
   def initialize
     @player1 = Player.new "player1"
     @player2 = Player.new "player2"
     @turn = 0
-    @questions = [ {q: "What does 5 plus 3 equal?", answer: 8},
-                  {q: "What does 2 plus 6 equal?", answer: 8},
-                  {q: "What does 4 plus 6 equal?", answer: 10},
-                  {q: "What does 3 plus 4 equal?", answer: 7},
-                  {q: "What does 3 plus 4 equal?", answer: 7},
-                  {q: "What does 3 plus 4 equal?", answer: 7},
-                  {q: "What does 3 plus 4 equal?", answer: 7} ]
+    @first_num
+    @second_num
+    @result
   end
 
   def game
-    # @questions.each do |question|
     while @player1.lives > 0 && @player2.lives > 0 do
-      question = @questions[rand(@questions.length)]
+      # turn evaluation
       if @turn % 2 == 0
         player = @player1
         @turn += 1
@@ -27,17 +22,22 @@ class Game
         @turn += 1
       end
 
-      puts "#{player.name}: #{question[:q]}"
+      # questions
+      @first_num = rand(1...20)
+      @second_num = rand(1...20)
+      @result = @first_num + @second_num
+      puts "#{player.name}: What does #{@first_num} plus #{@second_num} equal?"
       ans = gets.chomp
 
-      puts question[:answer].class
-      if question[:answer] == ans.to_i
+      # answer evaluation
+      if @result.to_i == ans.to_i
         puts "#{player.name}: Yes!!! You are correct!"
       else
         puts "#{player.name}: No! Seriously?!?!"
         player.lives -= 1
       end
 
+      # final messages
       if @player1.lives == 0
         puts "#{@player1.name} wins with a score of #{@player1.lives}/3!"
         puts "----- GAME OVER -----"
